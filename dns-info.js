2fbd48749feec0f4b945de2ab1c570bad4008840
Antti Mattila <antti.mattila@reaktor.fi>
2014-08-07 08:26:20 +0300

    Refactor: flip if to remove negation in if and name function

--- dns-info.js
+++ dns-info.js
@@ -2,7 +2,7 @@ var dns = require('native-dns')
 var Promise = require('bluebird')
 var _ = require('lodash')
 
-module.exports = function(domain) {
+module.exports = function fetchDnsInfo(domain) {
   var allTypes = Object.keys(dns.consts.NAME_TO_QTYPE).map(function(type) {
     return new Promise(function(resolve, reject) {
       dns.Request({
@@ -26,17 +26,17 @@ module.exports = function(domain) {
   return Promise.reduce(allTypes, function(result, response) {
     if (response.answer.length) {
       var ANY = 255
-      if (response.question[0].type != ANY) {
+      if (response.question[0].type === ANY) {
+        result.byAny = response.answer.map(function(entry) {
+          entry.type = dns.consts.QTYPE_TO_NAME[entry.type]
+          return _(entry).omit('name', 'class').value()
+        })
+      } else {
         var questionType = dns.consts.QTYPE_TO_NAME[response.question[0].type]
         var data = response.answer.map(function(entry) {
           return _(entry).omit('type', 'name', 'class').value()
         })
         result.byTypes.push({type: questionType, data: data})
-      } else {
-        result.byAny = response.answer.map(function(entry) {
-          entry.type = dns.consts.QTYPE_TO_NAME[entry.type]
-          return _(entry).omit('name', 'class').value()
-        })
       }
     }
     return result
